068cad9ed07c54bfaafa2820a885f59bcb542fc1
Denny Alquinta <dralquinta@google.com>
2024-08-19 16:58:37 -0400

    Adding build script

---
+++ build.sh
@@ -0,0 +1,10 @@
+ng build --prod
+mvn clean package
+
+gcloud builds submit --tag southamerica-west1-docker.pkg.dev/springboot-angular-sample/springboot-angular-sample/springboot-angular-sample
+
+gcloud run deploy springboot-angular-sample \
+  --image southamerica-west1-docker.pkg.dev/springboot-angular-sample/springboot-angular-sample/springboot-angular-sample \
+  --platform managed \
+  --region southamerica-west1 \
+  --allow-unauthenticated \
