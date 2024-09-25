548bf27037c7f7892c85fee401644eec57209155
Denny Alquinta <dralquinta@google.com>
2024-09-25 11:11:32 -0300

    Updating build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
+npm install @angular-devkit/build-angular
 ng build --configuration=production
-
 mvn clean package
 
 gcloud builds submit --tag southamerica-west1-docker.pkg.dev/springboot-angular-sample/springboot-angular-sample/springboot-angular-sample
